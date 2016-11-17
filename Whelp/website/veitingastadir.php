254dfc33b452d668e865d78d4953bd98440d8c59
kjartang <kjartanpain97@hotmail.com>
2016-11-17 13:26:11 +0000

    Add files via upload

--- Whelp/website/veitingastadir.php
+++ Whelp/website/veitingastadir.php
@@ -1,14 +1,13 @@
-<?php
-
-	$sql = "SELECT id, name, open FROM restaurants";
+<?php 
+	$sql = "SELECT * FROM restaurants";
 	$result = $conn->query($sql);
 
 	if ($result->num_rows > 0) {
-	     // output data of each row
-	     while($row = $result->fetch_assoc()) {
-	         echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " " . $row["open"] . "<br>";
-	     }
-	} else {
-	     echo "0 results";
-	}
-?>
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["location"]. " " . $row["open"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+ ?>
