d88db3ae60ba3b7c70446d291e5fcd01757833e4
kjartang <kjartanpain97@hotmail.com>
2016-11-17 13:55:02 +0000

    Add files via upload

---
+++ Whelp/website/nyskra.php
@@ -0,0 +1,12 @@
+<?php
+
+	$sql = "INSERT INTO user (name, username, email, password)
+	VALUES ($_POST["name"], $_POST["username"], $_POST["email"], $_POST["password"])";
+
+	if ($conn->query($sql) === TRUE) {
+	    echo "New record created successfully";
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+//http://stackoverflow.com/questions/15407802/using-a-submit-button-to-insert-an-entry-into-a-mysql-database-via-php
+?>
